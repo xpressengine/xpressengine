dc4fad7152e0f40b7ad5debeed0eae35acc50b15
khongchi <sungbum00@gmail.com>
2015-11-18 17:56:08 +0900

    #14 캡차 설정이 되어있지 않은 경우 capth를 활성화하지 못하도록 변경

---
+++ core/src/Xpressengine/Captcha/ConfigurationNotExistsException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ConfigurationNotExistsException class
+ *
+ * PHP version 5
+ *
+ * @category    Captcha
+ * @package     Xpressengine\Captcha
+ * @author      XE Team (developers) <developers@xpressengine.com>
+ * @copyright   2015 Copyright (C) NAVER <http://www.navercorp.com>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
+ * @link        http://www.xpressengine.com
+ */
+namespace Xpressengine\Captcha;
+
+use Xpressengine\Support\Exceptions\XpressengineException;
+
+/**
+ * 캡차 설정이 되어있지 않은 경우 발생하는 예외
+ *
+ * @category    Captcha
+ * @package     Xpressengine\Captcha
+ * @author      XE Team (developers) <developers@xpressengine.com>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
+ * @link        http://www.xpressengine.com
+ */
+class ConfigurationNotExistsException extends XpressengineException
+{
+    protected $message = '캡차 설정이 되어있지 않습니다. 설정파일(config/captcha.php)을 확인하십시오.';
+}
