4bab2ed5568e75e940030ee2051801c9daeb9c43
jhyeon1010 <jhyeon1010@naver.com>
2015-12-10 17:18:04 +0900

    add dynamic fields

---
+++ resources/views/dynamicField/boolean/default/create.blade.php
@@ -0,0 +1,2 @@
+<label class="__xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}">{{$config->get('label')}}</label>
+<input type="checkbox" name="{{$config->get('id') . 'boolean'}}" class="form-control __xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" value="1" />
---
+++ resources/views/dynamicField/text/default/search.blade.php
@@ -0,0 +1,1 @@
+<input name="{{$config->get('id') . 'Text'}}" class="form-control __xe_df __xe_df_text __xe_df_text_{{$config->get('id')}}" value="{{$text}}" placeholder="{{$config->get('label')}}"/>
