a5cc8fe48befdfc971a833f42d8bafcf2b509fc8
jhyeon1010 <jhyeon1010@naver.com>
2016-03-30 17:41:01 +0900

    seo 설정페이지 디자인 적용

--- resources/views/seo/setting.blade.php
+++ resources/views/seo/setting.blade.php
@@ -5,107 +5,72 @@
 @section('page_description')
     Search Engine Optimization setting
 @endsection
-<style>
-    .inpt_url em {margin-right:10px;font-size:20px}
-    .inpt_url .xe-input-text {width:90%}
-</style>
 
-<div class="panel">
-    <div class="panel-heading">
 <div class="row">
     <div class="col-sm-12">
-
-        <form method="post" action="{{ route('manage.seo.update') }}" enctype="multipart/form-data" data-rule="seoSetting">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-            <div class="admin_card">
-                <div class="card_tit">
-                    <div class="row">
-                        <p class="text-title">{{ xe_trans('xe::defaultSettings') }}</p>
-                    </div>
-                </div>
-                <div class="card_cont">
-                    <div class="row">
-                        <div class="col-sm-12 inpt_bd">
-                            <p class="text-title">{{ xe_trans('xe::siteImage') }} (1200x600, 600x315, 200x200)</p>
-                            <input type="file" class="xe-input-text" name="siteImage">
-                            @if($setting->getSiteImage())
-                                <br />
-                                {!! $setting->getSiteImage()->render(['responsive' => true]) !!}
-                            @endif
+        <div class="panel-group">
+            <form method="post" action="{{ route('manage.seo.update') }}" enctype="multipart/form-data" data-rule="seoSetting">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <div class="panel">
+                    <div class="panel-heading">
+                        <div class="pull-left">
+                            <h3 class="panel-title">{{ xe_trans('xe::defaultSettings') }}</h3>
                         </div>
                     </div>
-                </div>
-            </div>
-
-            <div class="admin_card">
-                <div class="card_tit">
-                    <div class="row">
-                        <p class="text-title">{{ xe_trans('xe::titleOptimization') }}</p>
-                    </div>
-                </div>
-                <div class="card_cont">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <p class="text-title">{{ xe_trans('xe::browserTitle') }}</p>
-                            {!! uio('langText', ['langKey'=> $setting->get('mainTitle'), 'name'=>'mainTitle']) !!}
-
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <label>{{ xe_trans('xe::siteImage') }} <small>1200x600, 600x315, 200x200</small></label>
+                            <div class="list-group-item">
+                                <input type="file" name="siteImage">
+                                @if($setting->getSiteImage())
+                                    <br />
+                                    {!! $setting->getSiteImage()->render(['responsive' => true]) !!}
+                                @endif
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <p class="text-title">{{ xe_trans('xe::browserSubTitle') }}</p>
-                            {!! uio('langText', ['langKey'=> $setting->get('subTitle'), 'name'=>'subTitle']) !!}
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>{{ xe_trans('xe::browserTitle') }}</label>
+                                    {!! uio('langText', ['langKey'=> $setting->get('mainTitle'), 'name'=>'mainTitle']) !!}
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>{{ xe_trans('xe::browserSubTitle') }}</label>
+                                    {!! uio('langText', ['langKey'=> $setting->get('subTitle'), 'name'=>'subTitle']) !!}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="admin_card">
-                <div class="card_tit">
-                    <div class="row">
-                        <p class="text-title">Meta Tag</p>
-                    </div>
-                </div>
-                <div class="card_cont">
-                    <div class="row">
-                        <div class="col-md-12 inpt_bd">
-                            <p class="text-title">{{ xe_trans('xe::siteKeyword') }}</p>
-                            <input type="text" class="xe-input-text" name="keywords" value="{{ $setting->get('keywords') ?: Input::old('keywords') }}">
+                        <div class="form-group">
+                            <label>{{ xe_trans('xe::metaTag') }} <small>{{ xe_trans('xe::inputSiteKeyword') }}</small></label>
+                            <input type="text" class="form-control" name="keywords" value="{{ $setting->get('keywords') ?: Input::old('keywords') }}">
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <p class="text-title">{{ xe_trans('xe::description') }}</p>
+                        <div class="form-group">
+                            <label>{{ xe_trans('xe::description') }} <small>{{ xe_trans('xe::inputSiteDescription') }}</small></label>
                             {!! uio('langText', ['langKey'=> $setting->get('description'), 'name'=>'description']) !!}
                         </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="admin_card">
-                <div class="card_tit">
-                    <div class="row">
-                        <p class="text-title">{{ xe_trans('xe::etc') }}</p>
-                    </div>
-                </div>
-                <div class="card_cont">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <p class="text-title">Twitter Username</p>
-                            <div class="inpt_url">
-                                <em class="txt_blue">@</em><input type="text" class="xe-input-text" name="twitterUsername" placeholder="username" value="{{ $setting->get('twitterUsername') ?: Input::old('twitterUsername') }}">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label>{{ xe_trans('xe::contents') }} {{ xe_trans('xe::owner') }} {{ xe_trans('xe::info') }} <small>{{ xe_trans('xe::inputSourceAddress', ['name' => 'twitter']) }}</small></label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon" id="basic-addon1">@</span>
+                                        <input type="text" id="item-url" name="twitterUsername" class="form-control" aria-describedby="basic-addon1" placeholder="Twitter Username" value="{{ $setting->get('twitterUsername') ?: Input::old('twitterUsername') }}">
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="panel-footer">
+                        <div class="pull-right">
+                            {{--<button type="button" class="btn btn-default">취소</button>--}}
+                            <button type="submit" class="btn btn-primary"><i class="xi-download"></i>{{ xe_trans('xe::submit') }}</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <div class="btn_group_all">
-                <button class="btn_setting blue">{{ xe_trans('xe::submit') }}</button>
-            </div>
-
-        </form>
+            </form>
+        </div>
     </div>
 </div>
 
-    </div>
-</div>
