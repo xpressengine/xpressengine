59e674dc59c1812a45a93e54f3ecafc1c417f64c
seungmanchoi <seungman@xeimac.local>
2016-04-26 13:20:21 +0900

    #124 메뉴 아이템 생성시 비활성화로 유지되는 이슈 수정.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -485,7 +485,7 @@ class MenuController extends Controller
                 'target' => $itemInput['itemTarget'],
                 'type' => $itemInput['selectedType'],
                 'ordering' => $itemInput['itemOrdering'],
-                'activated' => $itemInput['itemActivated'],
+                'activated' => isset($itemInput['itemActivated']) ? $itemInput['itemActivated'] : 0,
                 'parentId' => $itemInput['parent']
             ], $menuTypeInput);
 
