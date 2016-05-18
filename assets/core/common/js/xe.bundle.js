8bc9641c3298cd1f0048926d5538a02ad6ac838d
seungmanchoi <blueng.choi@gmail.com>
2016-05-18 16:31:36 +0900

    #147

--- assets/core/common/js/xe.bundle.js
+++ assets/core/common/js/xe.bundle.js
@@ -709,11 +709,15 @@ System.amdDefine('xe.component', [], function() {
     'use strict';
 
     //define시 필수 구현되어야 하는 object
-    var requireOptions = [
-            'name',
-            'getContents', 'setContents', 'addContents',
-            'initialize'
-        ],
+    var requireOptions = {
+            editorSettings: [
+                'name'
+            ],
+            interfaces: [
+                'initialize',
+                'getContents', 'setContents', 'addContents'
+            ]
+        },
         editorSet = {},
         editorOptionSet = {};
 
@@ -730,95 +734,104 @@ System.amdDefine('xe.component', [], function() {
             return editorSet[this.editorName].editorList[this.selector];
         },
         getContents: function() {
-            return editorSet[this.editorName].getContents.call(this.getInstance());
+            return editorSet[this.editorName].interfaces.getContents.call(this.getInstance());
         },
         setContents: function(text) {
-            editorSet[this.editorName].setContents.call(this.getInstance(), text);
+            editorSet[this.editorName].interfaces.setContents.call(this.getInstance(), text);
         },
         addContents: function(text) {
-            editorSet[this.editorName].addContents.call(this.getInstance(), text);
+            editorSet[this.editorName].interfaces.addContents.call(this.getInstance(), text);
         },
         addProps: function(obj) {
             for(var o in obj) {
                 this.getInstance().props[o] = obj[o];
             }
+        },
+        addComponents: function(components) {
+            editorSet[this.editorName].interfaces.addComponents.call(this.getInstance(), components);
         }
     };
 
-    var Editor = function(options) {
-        for(var o in options) {
-            this[o] = options[o];
+    var Editor = function(editorSettings, interfaces) {
+        this.name = editorSettings.name;
+        this.configs = editorSettings.configs;
+        this.editorList = [];
+
+        if(editorSettings.hasOwnProperty('plugins')
+            && editorSettings.plugins instanceof Array
+            && editorSettings.plugins.length > 0
+            && editorSettings.hasOwnProperty('addPlugins')) {
+            editorSettings.addPlugins(editorSettings.plugins);
+        }
+
+        for(var o in interfaces) {
+            this.interfaces[o] = interfaces[o];
         }
     };
 
     Editor.prototype = {
+        configs: {},
+        interfaces: {},
         create: function(sel, options) {
-            this.editorList[sel] = new instanceObj(this.name, sel, options);
-            this.initialize.call(this.editorList[sel], sel, options);
+            var options = $.extend(this.configs, options);
 
-            if(this.hasOwnProperty('components') && this.components.length > 0) {
-                this.addComponents.call(this.editorList[sel], this.components);
+            this.editorList[sel] = new instanceObj(this.name, sel, options);
+            this.interfaces.initialize.call(this.editorList[sel], sel, options);
+
+            if(this.interfaces.hasOwnProperty('components') && this.interfaces.components.length > 0) {
+                this.interfaces.addComponents.call(this.editorList[sel], this.interfaces.components);
             }
 
             return this.editorList[sel];
-        },
-        getContents: function() {
-            console.error('Editor.getContents');
-        },
-        setContents: function(text) {
-            console.error('Editor.setContents');
-        },
-        addContents: function(text) {
-            console.error('Editor.addContents');
-        },
-        addComponents: function(components) {
-
-        },
-        addPlugins: function(plugins) {
-
         }
     };
 
     var XEeditor = {
-        define: function(options) {
-            if(this.isValidOptions(options)) {
-                editorOptionSet[options.name] = options;
-                editorSet[options.name] = new Editor(options);
+        define: function(obj) {
+            var editorSettings = obj.editorSettings,
+                interfaces = obj.interfaces;
+
+            if(this.isValidOptions(editorSettings, interfaces)) {
+                editorOptionSet[editorSettings.name] = editorSettings;
+                editorSet[editorSettings.name] = new Editor(editorSettings, interfaces);
             }
         },
-        isValidOptions: function(options) {
+        isValidOptions: function(editorSettings, interfaces) {
             var valid = true;
-            for(var option in requireOptions) {
-                if(!options.hasOwnProperty(requireOptions[option])) {
-                    console.error('구현 필요 [fn:' + requireOptions[option] + ']');
+            for(var eSettings in requireOptions.editorSettings) {
+                if(!editorSettings.hasOwnProperty(requireOptions.editorSettings[eSettings])) {
+                    console.error('구현 필요 [editorSettings.' + requireOptions.editorSettings[eSettings] + ']');
                     valid = false;
-                }
-                
-                if(options.hasOwnProperty('components')
-                    && options.components instanceof Array
-                    && options.components.length > 0
-                    && !options.hasOwnProperty('addComponents')) {
-                    console.error('구현 필요 [fn:addComponents]');
-                }
-                
-                if(options.hasOwnProperty('plugins')
-                    && options.plugins instanceof Array
-                    && options.plugins.length > 0
-                    && !options.hasOwnProperty('addPlugins')) {
-                    console.error('구현 필요 [fn:addPlugins]');
                 }
             }
 
-            if(!!editorSet.hasOwnProperty(options.name)) {
-                console.error('등록된 에디터 있음 [' + options.name + ']');
+            for(var eInterface in requireOptions.interfaces) {
+                if(!interfaces.hasOwnProperty(requireOptions.interfaces[eInterface])) {
+                    console.error('구현 필요 [' + requireOptions.interfaces[eInterface] + ']');
+                    valid = false;
+                }
+            }
+
+            if(editorSettings.hasOwnProperty('plugins')
+                && editorSettings.plugins instanceof Array
+                && editorSettings.plugins.length > 0
+                && !editorSettings.hasOwnProperty('addPlugins')) {
+                console.error('구현 필요 [fn:addPlugins]');
+            }
+
+            if(interfaces.hasOwnProperty('components')
+                && interfaces.components instanceof Array
+                && interfaces.components.length > 0
+                && !interfaces.hasOwnProperty('addComponents')) {
+                console.error('구현 필요 [fn:addComponents]');
+            }
+
+            if(!!editorSet.hasOwnProperty(editorSettings.name)) {
+                console.error('등록된 에디터 있음 [' + editorSettings.name + ']');
                 valid = false;
             }
 
-            if(!valid) {
-                return false;
-            }
-
-            return true;
+            return (!valid)? false : true;
         },
         getEditor: function(name) {
             return editorSet[name];
@@ -828,93 +841,6 @@ System.amdDefine('xe.component', [], function() {
     exports.XEeditor = XEeditor;
 })(window);
 
-// <textarea name="test" id="editor1" cols="30" rows="10"></textarea>
-// <textarea name="test2" id="editor2" cols="30" rows="10"></textarea>
-
-
-// var components = [{
-//     name: 'Code',
-//     options: {
-//         label: 'Wrap code',
-//         command: 'wrapCode'
-//     },
-//     exec: function(editor) {
-//         editor.insertText( '```diagram\n' + editor.getSelection().getSelectedText() + '\n```' );
-//     }
-// }, {
-//     name: 'Diagram',
-//     options: {
-//         label: 'Wrap diagram',
-//         command: 'wrapDiagram'
-//     },
-//     exec: function (editor) {
-//         editor.insertText( '```diagram\n' + editor.getSelection().getSelectedText() + '\n```' );
-//     }
-// }
-// ];
-//
-// //ckeditor
-// XEeditor.define({
-//     name: 'editor.ckeditor',
-//     components: components,
-//     plugins: [],
-//     initialize: function (selector, options) {
-//
-//         var options = $.extend({
-//             "height": 200,
-//             "fileUpload":{
-//                 "upload_url": "/file/upload",
-//                 "source_url": "/file/source",
-//                 "download_url": "/file/download"
-//             },
-//             "suggestion":{
-//                 "hashtag_api": "/suggestion/hashTag",
-//                 "mention_api": "/suggestion/mention"
-//             }
-//         }, options);
-//
-//         var editor = CKEDITOR.replace(selector, options || {});
-//
-//         console.log('editor', editor);
-//         editor.on('change', function(e) {
-//             e.editor.updateElement();
-//         });
-//
-//         this.addProps({
-//             editor: editor
-//             , selector: selector
-//             , options: options
-//         });
-//     },
-//     getContents: function () {
-//         return CKEDITOR.instances[this.props.selector].getData();
-//     },
-//     setContents: function (text) {
-//         CKEDITOR.instances[this.props.selector].setData(text);
-//     },
-//     addContents: function (text) {
-//         CKEDITOR.instances[this.props.selector].insertHtml(text);
-//     },
-//     addComponents: function (components) {
-//         console.log("addComponents", components);
-//
-//         var editor = this.props.editor;
-//
-//         for(var i = 0, max = components.length; i < max; i += 1) {
-//             var component = components[i];
-//
-//             editor.ui.add( component.name, CKEDITOR.UI_BUTTON, component.options);
-//
-//             if(component.hasOwnProperty('options')
-//                 && component.options.hasOwnProperty('command')) {
-//                 editor.addCommand(component.options.command, {
-//                     exec: component.exec
-//                 });
-//             }
-//         }
-//     }
-// });
-//
 // //tinyMCE
 // XEeditor.define({
 //     name: 'editor.tinyMCE',
