417d0afc8b8dd894f60fb5acce4f4629f5885cde
seungmanchoi <blueng.choi@gmail.com>
2016-05-31 16:53:54 +0900

    #142 XE.pageModal 추가

--- resources/assets/core/xe-ui-component/js/xe-page.js
+++ resources/assets/core/xe-ui-component/js/xe-page.js
@@ -4,7 +4,7 @@
         src: {}
     };
 
-    var pageCommon = function() {
+    var _pageCommon = function() {
         return {
             init: function () {
                 self = this;
@@ -37,7 +37,6 @@
                             callbackFunc = callbackFunc[objStack[i]];
                         }
                     }
-
                 }
 
                 XE.pageModal({
@@ -76,20 +75,43 @@
                     }
                 }
             },
+
+            getModalTemplate: function() {
+                return [
+                    '<div class="xe-modal" data-use="xe-page">',
+                        '<div class="xe-modal-dialog ">',
+                            '<div class="xe-modal-content"></div>',
+                        '</div>',
+                    '</div>'
+                ].join("\n");
+            }
+        }.init();
+    }();
+
+    /**
+     * @description validtion
+     * */
+    var _validation = function() {
+        return {
             isValidPage: function(options) {
                 var isValid = true;
 
-                if(!options.hasOwnProperty('type') || ['get', 'post'].indexOf(options.type.toLowerCase()) === -1) {
-                    console.error('type [ get | post ]');
-                    isValid = false;
+                if(!options.hasOwnProperty('url') || options.url === '') {
+                    console.error('page: 필수 option [url]');
                 }
 
                 return isValid;
             },
-            isValidPageModal: function() {
+            isValidPageModal: function(options) {
+                var isValid = true;
 
+                if(!options.hasOwnProperty('url') || options.url === '') {
+                    console.error('pageModal: 필수 option [url]');
+                }
+
+                return isValid;
             }
-        }.init();
+        };
     }();
 
     /**
@@ -97,7 +119,70 @@
      * <pre>
      *     - target   : {string} css selector
      *     - url        : {stirng} html file path
-     *     - type       : {string} default 'post'
+     *     - type       : {string} method
+     * </pre>
+     * @param {function} callback
+     * */
+    var _page = function(options, callback) {
+
+        var $target = $(options.target);
+
+        XE.Request[options.type](options.url, options.data || {}, function(data) {
+
+            var assets = data['XE_ASSET_LOAD'],
+                css = assets['css'] || [],
+                js = assets['js'] || [],
+                html = data.result,
+                cssLen = css.length,
+                jsLen = js.length;
+
+
+            var next = function() {
+                $target.html(html);
+
+                if(callback) {
+                    callback();
+                }
+            };
+
+            var loadDone = _pageCommon.loadDone(cssLen, jsLen, next);
+
+            if(cssLen > 0) {
+                for(var i = 0, max = cssLen; i < max; i += 1) {
+                    if(!_assets.hasOwnProperty(css[i])) {
+                        _pageCommon.cssLoad(css[i], loadDone, loadDone);
+                    }else {
+                        loadDone();
+                    }
+                }
+            }
+
+            if(jsLen > 0) {
+                for(var i = 0, max = jsLen; i < max; i += 1) {
+                    if(!_assets.hasOwnProperty(css[i])) {
+                        _pageCommon.jsLoad(js[i], loadDone, loadDone);
+                    }else {
+                        loadDone();
+                    }
+                }
+            }
+
+            if((cssLen + jsLen) === 0) {
+                next();
+            }
+
+        }, 'json');
+
+    };
+
+    /**
+     *
+     * @param {string} url
+     * @param {string} target selector
+     * @param {object} options
+     * <pre>
+     *     - data : request parameters
+     *     - type : http method (get | post) default 'get'
      * </pre>
      * @param {function} callback
      * @description
@@ -105,75 +190,70 @@
      *     selecor영역에 html을 로드하여 보여준다. html 랜더링 전에 assets파일들의 로드가 선행된다.
      *
      *     동작 순서
-     *     1)css로드
+     *     1)css로드 + js로드
      *     2)html string append
-     *     3)js로드
-     *     4)callback 실행
+     *     3)callback 실행
      * </pre>
      * */
-    XE.page = function(options, callback) {
+    XE.page = function(url, target, options, callback) {
+        var options = $.extend(options, {
+            url: url,
+            target: target,
+            type: options.type || 'get'
+        });
 
-        var $target = $(options.target);
-
-        if(pageCommon.isValidPage(options)) {
-            XE.Request[options.type](options.url, {}, function(data) {
-
-                var assets = data['XE_ASSET_LOAD'],
-                    css = assets['css'] || [],
-                    js = assets['js'] || [],
-                    html = data.result,
-                    cssLen = css.length,
-                    jsLen = js.length;
-
-
-                var next = function() {
-                    $target.html(html);
-
-                    if(callback) {
-                        callback();
-                    }
-                };
-
-                var loadDone = pageCommon.loadDone(cssLen, jsLen, next);
-
-                if(cssLen > 0) {
-                    for(var i = 0, max = cssLen; i < max; i += 1) {
-                        if(!_assets.hasOwnProperty(css[i])) {
-                            pageCommon.cssLoad(css[i], loadDone, loadDone);
-                        }else {
-                            loadDone();
-                        }
-                    }
-                }
-
-                if(jsLen > 0) {
-                    for(var i = 0, max = jsLen; i < max; i += 1) {
-                        if(!_assets.hasOwnProperty(css[i])) {
-                            pageCommon.jsLoad(js[i], loadDone, loadDone);
-                        }else {
-                            loadDone();
-                        }
-                    }
-                }
-
-                if((cssLen + jsLen) === 0) {
-                    next();
-                }
-
-            }, 'json');
+        if(_validation.isValidPage(options)) {
+            _page(options, callback);
         }
     };
 
     /**
+     * @param {string} url
      * @param {object} options
      * <pre>
-     *     - url        : {string} html file path
-     *     - type       : {string} default 'post'
+     *     - data : request parameters
+     *     - type : http method (get | post) default 'get'
      * </pre>
      * @param {function} callback
+     * @description
+     * <pre>
+     *     modal을 실행하여 .xe-modal-content 영역에 html을 로드하여 보여준다. html 랜더링 전에 assets파일들의 로드가 선행된다.
+     *
+     *     동작 순서
+     *     1)css로드 + js로드
+     *     2)html string append
+     *     3)callback 실행
+     *     4)modal show
+     * </pre>
      * */
-    XE.pageModal = function(options, callback) {
+    XE.pageModal = function(url, options, callback) {
 
+        var options = $.extend(options, {
+            target: ".xe-modal[data-use=xe-page] .xe-modal-content",
+            url: url,
+            type: options.type || 'get'
+        });
+
+        if(_validation.isValidPageModal(options)) {
+            var $modal = $(".xe-modal[data-use=xe-page]");
+
+            if($modal.length > 0) {
+                $modal.find(".xe-modal-content").empty();
+            }else {
+                var modalTemplate = _pageCommon.getModalTemplate();
+
+                $("body").append(modalTemplate);
+            }
+
+            _page(options, function() {
+                if(callback) {
+                    callback();
+                }
+
+                $(".xe-modal[data-use=xe-page]").xeModal();
+            });
+
+        }
     };
 
 })(jQuery, window, XE);
