bc8a0f3ce4d0593ff6cfcb62f358a3cd39e06203
akasima <akasima@nate.com>
2016-06-12 12:57:42 +0900

    #178 core blade 다국어 처리

--- resources/views/widget/list.blade.php
+++ resources/views/widget/list.blade.php
@@ -4,8 +4,8 @@
     <div class="col-md-12">
         <input type="hidden" name="targetId" id="targetId" value="{{$targetId}}">
         <div class="form-group">
-            <label for="">위젯 선택</label>
-            <p class="desc-text">원하시는 위젯을 선택하여 주십시오.</p>
+            <label for="">{{xe_trans('xe::selectWidget}}</label>
+            <p class="desc-text">{{xe_trans('xe::selectWidgetDescription')}}</p>
             <select id="widget" name="widget" class="form-control">
                 @foreach ($widgets as $id => $widget)
                     <option value="{{ $id }}" >
@@ -17,7 +17,7 @@
         <?php /*
         <a class="btn btn-default" href="/manage/module/pluginA@board/dynamicField/freeboard">list</a>
     */?>
-        <button type="button" class="btn btn-primary __xe_btn_getSetup">Next</button>
+        <button type="button" class="btn btn-primary __xe_btn_getSetup">{{xe_trans('xe::next')}}</button>
     </div>
 </form>
 </div>
