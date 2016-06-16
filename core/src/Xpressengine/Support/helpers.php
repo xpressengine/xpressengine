ab6f25984abfaab8fb3c2e025ab39a616765d30e
jhyeon1010 <jhyeon1010@naver.com>
2016-06-16 16:15:41 +0900

    apiRender 추가 data 전달 처리

--- core/src/Xpressengine/Support/helpers.php
+++ core/src/Xpressengine/Support/helpers.php
@@ -99,11 +99,12 @@ if (function_exists('apiRender') === false) {
      *
      * @package Xpressengine\Presenter
      *
-     * @param string $id   view id
-     * @param array  $data data
+     * @param string $id           view id
+     * @param array  $data         data
+     * @param array  $responseData data
      * @return mixed
      */
-    function apiRender($id, array $data = [])
+    function apiRender($id, array $data = [], array $responseData = [])
     {
         XePresenter::htmlRenderPartial();
 
@@ -117,6 +118,7 @@ if (function_exists('apiRender') === false) {
 
         return XePresenter::makeApi([
             'result' => (string)$result,
+            'data' => $responseData,
             'XE_ASSET_LOAD' => [
                 'css' => \Xpressengine\Presenter\Html\Tags\CSSFile::getFileList(),
                 'js' => \Xpressengine\Presenter\Html\Tags\JSFile::getFileList(),
