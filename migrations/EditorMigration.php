57e13fc55c6c08e683fe2e9e3c2e675e02634823
jhyeon1010 <jhyeon1010@naver.com>
2016-06-29 14:46:04 +0900

    migration interface 오류 수정

--- migrations/EditorMigration.php
+++ migrations/EditorMigration.php
@@ -21,9 +21,9 @@ class EditorMigration implements Migration
      *
      * @return mixed
      */
-    public function checkInstall()
+    public function checkInstalled()
     {
-        // TODO: Implement checkInstall() method.
+        // TODO: Implement checkInstalled() method.
     }
 
     /**
@@ -39,23 +39,23 @@ class EditorMigration implements Migration
     /**
      * checkUpdate
      *
-     * @param string $currentVersion current version
+     * @param string $installedVersion current version
      *
      * @return mixed
      */
-    public function checkUpdate($currentVersion)
+    public function checkUpdated($installedVersion = null)
     {
-        // TODO: Implement checkUpdate() method.
+        // TODO: Implement checkUpdated() method.
     }
 
     /**
      * update
      *
-     * @param string $currentVersion current version
+     * @param string $installedVersion current version
      *
      * @return mixed
      */
-    public function update($currentVersion)
+    public function update($installedVersion = null)
     {
         // TODO: Implement update() method.
     }
