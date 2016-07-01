15d89b61f4225575c0d238178c1223bd0eb893fc
jhyeon1010 <jhyeon1010@naver.com>
2016-07-01 16:49:53 +0900

    file view skin 열린 상태 처리

--- resources/views/editor/skin/files.blade.php
+++ resources/views/editor/skin/files.blade.php
@@ -2,7 +2,7 @@
 
 <div class="bd_file_list">
     <!-- [D] 클릭시 클래스 on 적용 -->
-    <a href="#" class="bd_btn_file"><i class="xi-clip"></i><span class="xe-sr-only">file attached list</span> <strong class="bd_file_num">{{ count($files) }}</strong></a>
+    <a href="#" class="bd_btn_file on"><i class="xi-clip"></i><span class="xe-sr-only">file attached list</span> <strong class="bd_file_num">{{ count($files) }}</strong></a>
     <ul>
         @foreach($files as $file)
             <li><a href="{{ route('editor.file.download', ['instanceId' => $instanceId, 'id' => $file->id])}}"><i class="xi-download"></i> {{ $file->clientname }} <span class="file_size">({{ bytes($file->size) }})</span></a></li>
