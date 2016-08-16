70a3e85d10ad040d7661de8ee9e02303e173b383
bnu <bnufactory@gmail.com>
2016-08-16 16:05:35 +0900

    Fix #252 파일의 확장자 변경으로 인해 기본 프로필 이미지가 잘못 나오는 문제 고침

--- config/xe.php
+++ config/xe.php
@@ -283,7 +283,7 @@ return [
             ]
         ],
         'profileImage' => [
-            'default' => 'assets/core/member/img/default_avatar.gif',
+            'default' => 'assets/core/member/img/default_avatar.jpg',
             'size' => ['width' => 240, 'height' => 240],
             'storage' => [
                 'disk' => 'local',
