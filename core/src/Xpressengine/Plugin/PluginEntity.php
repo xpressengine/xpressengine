591ccda4b6e4341458cd28b1ad15debddf88341e
khongchi <sungbum00@gmail.com>
2016-08-17 17:21:57 +0900

    #254 plugin update시 버전정보 사용 안함

--- core/src/Xpressengine/Plugin/PluginEntity.php
+++ core/src/Xpressengine/Plugin/PluginEntity.php
@@ -322,16 +322,13 @@ class PluginEntity implements Arrayable, Jsonable
     }
 
     /**
-     * 플러그인의 설치버전과 현재버전을 비교한다. 플러그인의 새로운 업데이트가 서버에 다운로드 되어 있는 상태인지 확인한다.
+     * 플러그인의 새로운 업데이트가 서버에 다운로드 되어 있는 상태인지 확인한다.
      *
      * @return boolean 설치가 필요할 경우 true를 반환
      */
     public function needUpdateInstall()
     {
         $installedVersion = $this->getInstalledVersion();
-        if ($installedVersion === $this->getVersion()) {
-            return false;
-        }
         return !$this->checkInstalled() || !$this->checkUpdated($installedVersion);
     }
 
