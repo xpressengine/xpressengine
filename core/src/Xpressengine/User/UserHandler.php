97a8ac54a0bef813b5dead0ef6c5b02a335dc50c
khongchi <sungbum00@gmail.com>
2017-08-30 17:55:15 +0900

    회원삭제시 프로필 이미지 삭제처리

--- core/src/Xpressengine/User/UserHandler.php
+++ core/src/Xpressengine/User/UserHandler.php
@@ -346,13 +346,15 @@ class UserHandler
         // delete user's accounts
         $this->accounts()->deleteByUserIds($userIds);
 
-        // resolve group
         foreach ($users as $user) {
             // except user from user's groups
             $user->groups()->detach();
             $user->delete();
+
+            // remove profle image
+            $this->imageHandler->removeUserProfileImage($user);
         }
-        // todo: remove profile image
+
     }
 
     /**
