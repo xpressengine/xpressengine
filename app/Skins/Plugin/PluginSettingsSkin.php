f793719a9083fd3ce3acb7a6ca2409117423684e
khongchi <sungbum00@gmail.com>
2017-08-31 11:43:40 +0900

    #550 editor, editortool 타입 추가

--- app/Skins/Plugin/PluginSettingsSkin.php
+++ app/Skins/Plugin/PluginSettingsSkin.php
@@ -124,6 +124,8 @@ class PluginSettingsSkin extends GenericSkin
             'widget' => 'danger',
             'module' => 'danger',
             'uiobject' => 'primary',
+            'editor' => 'primary',
+            'editortool' => 'primary',
             'FieldType' => 'default',
             'FieldSkin' => 'default',
         ]);
