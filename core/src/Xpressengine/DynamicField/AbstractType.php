048b99cb8119dad15e8d55d39aa84fd8f205da57
akasima <akasima@nate.com>
2017-09-26 14:40:02 +0900

    #574 test 오류 수정

--- core/src/Xpressengine/DynamicField/AbstractType.php
+++ core/src/Xpressengine/DynamicField/AbstractType.php
@@ -681,7 +681,7 @@ abstract class AbstractType implements ComponentInterface
         $insertParam = [];
         $insertParam['dynamic_field_target_id'] = $args['id'];
         $insertParam['revision_id'] = $args['revision_id'];
-        $insertParam['revision_no'] = $args['revisionNo'];
+        $insertParam['revision_no'] = $args['revision_no'];
         foreach ($this->getColumns() as $column) {
             $key = $this->config->get('id') . '_' . $column->name;
 
