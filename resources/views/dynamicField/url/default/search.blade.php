41ad7589b6503f4ecfd2cb6b44028eb63cbcdb38
akasima <akasima@nate.com>
2017-11-10 06:52:46 +0900

    #591 email, url 필드타입 오류 수정

--- resources/views/dynamicField/url/default/search.blade.php
+++ resources/views/dynamicField/url/default/search.blade.php
@@ -1,1 +1,1 @@
-<input name="{{$key['email']}}" class="form-control __xe_df __xe_df_email __xe_df_email_{{$config->get('id')}}" value="{{$data['email']}}" placeholder="{{xe_trans($config->get('label'))}}"/>
+<input name="{{$key['url']}}" class="form-control __xe_df __xe_df_url __xe_df_url_{{$config->get('id')}}" value="{{$data['url']}}" placeholder="{{xe_trans($config->get('label'))}}"/>
