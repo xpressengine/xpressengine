27d86cf9dfd441d5d5d37a5a4487fb5583a43f17
jhyeon1010 <jhyeon1010@naver.com>
2017-11-16 11:13:09 +0900

    add data-valid-name

--- resources/views/user/skins/default/auth/register/forms/agreements.blade.php
+++ resources/views/user/skins/default/auth/register/forms/agreements.blade.php
@@ -1,5 +1,5 @@
 <label class="xe-label">
-    <input type="checkbox" name="agree">
+    <input type="checkbox" name="agree" data-valid-name="{{xe_trans('xe::siteTermsUse')}}">
     <span class="xe-input-helper"></span>
     <span class="xe-label-text">
         {!! xe_trans('xe::agreeSiteTermsUseAndSitePrivacyPolicy', ['termLink' => '#', 'policyLink' => '#']) !!}
--- resources/views/user/skins/default/auth/register/forms/default.blade.php
+++ resources/views/user/skins/default/auth/register/forms/default.blade.php
@@ -1,15 +1,15 @@
 <h4>기본 정보 입력</h4>
 <div class="auth-group">
     <label for="email" class="xe-sr-only">{{xe_trans('xe::email')}}</label>
-    <input type="text" id="email" class="xe-form-control" placeholder="{{xe_trans('xe::email')}}" name="email" value="{{ old('email') }}">
+    <input type="text" id="email" class="xe-form-control" placeholder="{{xe_trans('xe::email')}}" name="email" value="{{ old('email') }}" data-valid-name="{{xe_trans('xe::email')}}">
 </div>
 <div class="auth-group">
     <label for="name" class="xe-sr-only">{{xe_trans('xe::name')}}</label>
-    <input type="text" id="name" class="xe-form-control" placeholder="{{xe_trans('xe::name')}}" name="display_name" value="{{ old('display_name') }}">
+    <input type="text" id="name" class="xe-form-control" placeholder="{{xe_trans('xe::name')}}" name="display_name" value="{{ old('display_name') }}" data-valid-name="{{xe_trans('xe::name')}}">
 </div>
 <div class="auth-group">
     <label for="pwd" class="xe-sr-only">{{xe_trans('xe::password')}}</label>
-    <input type="password" id="pwd" class="xe-form-control" placeholder="{{xe_trans('xe::password')}}" name="password" onfocus="$('.__xe_password-description').slideDown();">
+    <input type="password" id="pwd" class="xe-form-control" placeholder="{{xe_trans('xe::password')}}" name="password" onfocus="$('.__xe_password-description').slideDown();" data-valid-name="{{xe_trans('xe::password')}}">
     <em style="display: none;" class="text-message __xe_password-description">{{ xe_trans($passwordLevel['description']) }}</em>
 </div>
 <div class="auth-group">
