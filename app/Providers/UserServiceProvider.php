23644b640e395339728f4420dd8d129826a4e55d
jhyeon1010 <jhyeon1010@naver.com>
2018-01-04 12:31:57 +0900

    remove password token repo register

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -163,25 +163,6 @@ class UserServiceProvider extends ServiceProvider
      */
     protected function registerTokenRepository()
     {
-        // register password-token repository
-        $this->app->singleton(
-            'auth.password.tokens',
-            function ($app) {
-                $connection = $app['xe.db']->connection('user');
-
-                // The database token repository is an implementation of the token repository
-                // interface, and is responsible for the actual storing of auth tokens and
-                // their e-mail addresses. We will inject this table and hash key to it.
-                $table = $app['config']['auth.password.table'];
-
-                $key = $app['config']['app.key'];
-
-                $expire = $app['config']->get('auth.password.expire', 60);
-
-                return new DatabaseTokenRepository($connection, $table, $key, $expire);
-            }
-        );
-
         // register register-token repository
         $this->app->singleton(RegisterTokenRepository::class, function ($app) {
             $connection = $app['xe.db']->connection('user');
