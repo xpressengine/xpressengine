45cbabd7e8531789735ab51097c57ad01b3e2859
jhyeon1010 <jhyeon1010@naver.com>
2018-02-19 18:35:48 +0900

    #545 인증메일 링크를 통해 가입페이지로 이동시 인증코드 영역 노출

--- resources/views/user/skins/default/auth/register/forms/confirm.blade.php
+++ resources/views/user/skins/default/auth/register/forms/confirm.blade.php
@@ -1,17 +1,13 @@
 <input type="hidden" name="register_token" value="{{ $token->id }}">
-@if($code !== null)
-    <input type="hidden" name="code" value="{{ $code }}">
-@else
-    <h4>{{ xe_trans('xe::inputEmailConfirmCode') }}</h4>
-    <div>
-        <div class="auth-group">
-            <p>{{ xe_trans('xe::inputEmailConfirmCodeDescription', ['email' => $token->email]) }}</p>
-            <label for="code" class="xe-sr-only">{{xe_trans('xe::confirmCode')}}</label>
-            <input type="text" id="code" class="xe-form-control" placeholder="{{xe_trans('xe::confirmCode')}}" name="code">
-        </div>
+<h4>{{ xe_trans('xe::inputEmailConfirmCode') }}</h4>
+<div>
+    <div class="auth-group">
+        <p>{{ xe_trans('xe::inputEmailConfirmCodeDescription', ['email' => $token->email]) }}</p>
+        <label for="code" class="xe-sr-only">{{xe_trans('xe::confirmCode')}}</label>
+        <input type="text" id="code" class="xe-form-control" placeholder="{{xe_trans('xe::confirmCode')}}" name="code" value="{{ $code ?: '' }}">
     </div>
-    <hr>
-@endif
+</div>
+<hr>
 
 {{
 XeFrontend::html('email.setter')->content("
