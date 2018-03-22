5b95148bb647f59cecb4681dea3e94a0c07df7ec
bnu <bnufactory@gmail.com>
2018-03-22 15:45:16 +0900

    #718 swiper2 임시 이동 - 관리페이지 플러그인 관리. 스크린샷 뷰어에 사용 됨

--- app/Skins/Plugin/PluginSettingsSkin.php
+++ app/Skins/Plugin/PluginSettingsSkin.php
@@ -94,10 +94,10 @@ class PluginSettingsSkin extends GenericSkin
     protected function show()
     {
         app('xe.frontend')->js([
-           'assets/vendor/swiper2/idangerous.swiper.js',
+           'assets/core/plugin/swiper2/idangerous.swiper.min.js',
            'assets/core/plugin/js/plugin.js'
         ])->appendTo('head')->load();
-        app('xe.frontend')->css('assets/vendor/swiper2/idangerous.swiper.css')
+        app('xe.frontend')->css('assets/core/plugin/swiper2/idangerous.swiper.css')
             ->before('assets/core/settings/css/admin.css')->load();
 
         return $this->renderBlade();
