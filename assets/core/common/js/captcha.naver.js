af45dc3ad8a669d537e007d874cd9b254b8b5ee0
bnu <bnufactory@gmail.com>
2018-04-09 12:06:08 +0900

    MINIFY

--- assets/core/common/js/captcha.naver.js
+++ assets/core/common/js/captcha.naver.js
@@ -1,11 +1,13 @@
+// @FIXME 이동
+// @FIXME i? k?
 var naverReissue = function (i, k) {
-  $.ajax({
-    url: xeBaseURL + '/captcha/naver/reissue',
+  window.jQuery.ajax({
+    url: window.xeBaseURL + '/captcha/naver/reissue',
     type: 'get',
     dataType: 'json',
     success: function (json) {
-      $(k).val(json.key)
-      $(i).attr('src', json.img)
+      window.jQuery(k).val(json.key)
+      window.jQuery(i).attr('src', json.img)
     }
   })
 }
