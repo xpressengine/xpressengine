db6b47a2f8f83c152f4c4d862832dac052cd68af
bnu <bnufactory@gmail.com>
2018-06-29 15:08:28 +0900

    fix #800 htmlpurifier 설정이 반영되지 못하는 문제 고침

--- config/purifier.php
+++ config/purifier.php
@@ -2,13 +2,13 @@
 
 return [
     'encoding' => 'UTF-8',
-    'finalize' => true,
+    'finalize' => false,
     'cachePath' => storage_path('framework/htmlpurifier/middleware'),
     'settings' => [
         'default' => [
             'Core.RemoveInvalidImg' => false,
             'HTML.AllowedModules' => 'CommonAttributes,Hypertext,Image,List,StyleAttribute,Tables'.
-                ',Text,Structure,Legacy,NonXMLCommonAttributes,XMLCommonAttributes',
+                ',Text,Structure,Legacy,NonXMLCommonAttributes,XMLCommonAttributes,TargetBlank,Presentation',
             'AutoFormat.AutoParagraph' => true,
             'AutoFormat.RemoveEmpty' => true
         ]
