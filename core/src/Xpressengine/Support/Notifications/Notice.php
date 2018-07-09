3453bec896dd6f023b10dd1c63e8fb377d3f6d98
akasima-mac18 <osh@xef.kr>
2018-07-09 17:27:32 +0900

    #806 add notification for sending notice email

---
+++ core/src/Xpressengine/Support/Notifications/Notice.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Notice.php
+ *
+ * PHP version 7
+ *
+ * @category    User
+ * @package     Xpressengine\Support
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2015 Copyright (C) NAVER Corp. <http://www.navercorp.com>
+ * @license     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPL-2.1
+ * @link        https://xpressengine.io
+ */
+
+namespace Xpressengine\Support\Notifications;
+
+use Xpressengine\Support\CustomizableMailNotification;
+
+/**
+ * Class Notice
+ *
+ * @category    Support
+ * @package     Xpressengine\Support
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2015 Copyright (C) NAVER Corp. <http://www.navercorp.com>
+ * @license     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPL-2.1
+ * @link        https://xpressengine.io
+ */
+class Notice extends CustomizableMailNotification
+{
+    /**
+     * Receive email
+     *
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * title for email contents
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * email content
+     *
+     * @var string
+     */
+    protected $contents;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param string $email    email
+     * @param string $title    title
+     * @param string $contents content
+     */
+    public function __construct($email, $title, $contents)
+    {
+        $this->email = $email;
+        $this->title = $title;
+        $this->contents = $contents;
+    }
+
+    /**
+     * Get data for message
+     *
+     * @param mixed $notifiable notifiable
+     * @return array
+     */
+    protected function getData($notifiable)
+    {
+        return ['mail' => $this->email, 'title' => $this->title, 'contents' => $this->contents];
+    }
+
+    /**
+     * Get the default view name.
+     *
+     * @return string
+     */
+    public function getDefaultView()
+    {
+        return 'emails.notice';
+    }
+
+    /**
+     * Get the default subject for message.
+     *
+     * @param mixed $notifiable notifiable
+     * @return string
+     */
+    public function getDefaultSubject($notifiable)
+    {
+        return xe_trans(app('xe.site')->getSiteConfig()->get('site_title')).' '.xe_trans('xe::notification');
+    }
+
+    /**
+     * Set subject resolver to null
+     */
+    public static function setSubjectResolverToNull()
+    {
+        static::$subjectResolver = null;
+    }
+}
