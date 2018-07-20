76ee9b36fc0cccbb29666de3816701ba355be409
woongbin <fzwoongbin@naver.com>
2018-07-20 17:17:53 +0900

    comment 추가

--- core/src/Xpressengine/User/Events/UserRetrievedEvent.php
+++ core/src/Xpressengine/User/Events/UserRetrievedEvent.php
@@ -1,15 +1,31 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sirwoongke
- * Date: 2018. 7. 3.
- * Time: 11:18
+ * UserRetrievedEvent.php
+ *
+ * PHP version 7
+ *
+ * @category    User
+ * @package     Xpressengine\User
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2015 Copyright (C) NAVER Corp. <http://www.navercorp.com>
+ * @license     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPL-2.1
+ * @link        https://xpressengine.io
  */
 
 namespace Xpressengine\User\Events;
 
 use Xpressengine\User\Models\User;
 
+/**
+ * UserRetrievedEvent
+ *
+ * @category    User
+ * @package     Xpressengine\User
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2015 Copyright (C) NAVER Corp. <http://www.navercorp.com>
+ * @license     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPL-2.1
+ * @link        https://xpressengine.io
+ */
 class UserRetrievedEvent
 {
     public $user;
