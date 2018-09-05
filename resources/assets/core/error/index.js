f6e137b9f31d668900bd989942d650de4f40e344
bnu <bnufactory@gmail.com>
2018-09-05 18:14:36 +0900

    XeError에 make-error 패키지를 사용 - instanceof 사용 불가한 문제 등 해결 - https://www.npmjs.com/package/make-error

--- resources/assets/core/error/index.js
+++ resources/assets/core/error/index.js
@@ -1,11 +1,15 @@
-export default class XeError extends Error {
+import { BaseError } from 'make-error'
+
+/**
+ * XeError
+ * @module XeError
+ * @extends BaseError
+ * @see {@link https://www.npmjs.com/package/make-error|npm make-error(BaseError)}
+ */
+class XeError extends BaseError {
   constructor (message) {
     super(message)
-
-    this.name = 'XeError'
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, this.constructor)
-    }
   }
 }
+
+export default XeError
