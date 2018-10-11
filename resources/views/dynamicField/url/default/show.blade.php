591dd1696bd072a39df0b894cab3bc20b3a13b96
sirwoongke <fzwoongbin@naver.com>
2018-10-11 12:10:11 +0900

    #886 url dynamic field 잘 못 지정되어 있는 type 수정

--- resources/views/dynamicField/url/default/show.blade.php
+++ resources/views/dynamicField/url/default/show.blade.php
@@ -1,4 +1,4 @@
 <div class="xe-form-group xe-dynamicField">
-    <label class="__xe_df __xe_df_email __xe_df_url_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
-    <span class="__xe_df __xe_df_email __xe_df_url_{{$config->get('id')}}">{{$data['url']}}</span>
+    <label class="__xe_df __xe_df_url __xe_df_url_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
+    <span class="__xe_df __xe_df_url __xe_df_url_{{$config->get('id')}}">{{$data['url']}}</span>
 </div>
