e07ba959e1b617098bd4ed622886b8dd27b76d4d
bnu <bnufactory@gmail.com>
2018-11-12 18:43:02 +0900

    boolean dynamicField 체크박스 디자인 정리

--- resources/views/dynamicField/boolean/default/edit.blade.php
+++ resources/views/dynamicField/boolean/default/edit.blade.php
@@ -1,5 +1,10 @@
 <div class="xe-form-group xe-dynamicField">
-    <label class="__xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
+    <div>
+        <label class="xe-label">
+            <input type="checkbox" name="{{$key['boolean']}}"class="xe-form-control __xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" value="1" @if($data['boolean'] === 1) checked @endif data-valid-name="{{ xe_trans($config->get('label')) }}">
+            <span class="xe-input-helper"></span>
+            <span class="xe-label-text">{{ xe_trans($config->get('label')) }}</span>
+        </label>
+    </div>
     @if ($config->get('skinDescription') !== '')<small>{{$config->get('skinDescription')}}</small>@endif
-    <input type="checkbox" name="{{$key['boolean']}}" class="xe-form-control __xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" value="1" @if($data['boolean'] === 1) checked @endif data-valid-name="{{ xe_trans($config->get('label')) }}" />
 </div>
