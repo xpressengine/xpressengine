7d5fc4a03f015d5cef97e5add172f37d22fd0037
jhyeon1010 <jhyeon1010@naver.com>
2018-11-28 21:34:56 +0900

    #908 DF boolean 타입 radio 적용

--- resources/views/dynamicField/boolean/default/create.blade.php
+++ resources/views/dynamicField/boolean/default/create.blade.php
@@ -1,10 +1,28 @@
 <div class="xe-form-group xe-dynamicField">
     <div>
-        <label class="xe-label">
-            <input type="checkbox" name="{{$key['boolean']}}"class="xe-form-control __xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" value="1" data-valid-name="{{ xe_trans($config->get('label')) }}">
-            <span class="xe-input-helper"></span>
-            <span class="xe-label-text">{{ xe_trans($config->get('label')) }}</span>
-        </label>
+        @if($config->get('required'))
+            <div class="xe-form-inline">
+                <label class="xe-label">
+                    <strong>{{ xe_trans($config->get('label')) }}</strong>
+                </label>
+                <label class="xe-label">
+                    <input type="radio" name="{{$key['boolean']}}" class="__xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" value="1" data-valid-name="{{ xe_trans($config->get('label')) }}">
+                    <span class="xe-input-helper"></span>
+                    <span class="xe-label-text">{{ $config->get('radioLabelYes') ?: xe_trans('xe::yes') }}</span>
+                </label>
+                <label class="xe-label">
+                    <input type="radio" name="{{$key['boolean']}}" class="__xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" value="0" data-valid-name="{{ xe_trans($config->get('label')) }}">
+                    <span class="xe-input-helper"></span>
+                    <span class="xe-label-text">{{ $config->get('radioLabelNo') ?: xe_trans('xe::no') }}</span>
+                </label>
+            </div>
+        @else
+            <label class="xe-label">
+                <input type="checkbox" name="{{$key['boolean']}}" class="xe-form-control __xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" value="1" data-valid-name="{{ xe_trans($config->get('label')) }}">
+                <span class="xe-input-helper"></span>
+                <span class="xe-label-text">{{ xe_trans($config->get('label')) }}</span>
+            </label>
+        @endif
     </div>
     @if ($config->get('skinDescription') !== '')<small>{{$config->get('skinDescription')}}</small>@endif
 </div>
--- resources/views/dynamicField/boolean/default/settings.blade.php
+++ resources/views/dynamicField/boolean/default/settings.blade.php
@@ -1,1 +1,17 @@
-<input type="text" name="skinDescription" placeholder="skin description" class="form-control" value="{{$config != null ? $config->get('skinDescription') : ''}}" />
+
+<div class="xe-form-group">
+    <label>{{ xe_trans('xe::view') }} Text</label>
+    <div class="xe-row">
+        <div class="xe-col-md-6">
+            <input type="text" name="radioLabelYes" placeholder="{{ xe_trans('xe::yes') }}" class="form-control" value="{{$config ? $config->get('radioLabelYes') : ''}}" />
+        </div>
+        <div class="xe-col-md-6">
+            <input type="text" name="radioLabelNo" placeholder="{{ xe_trans('xe::no') }}" class="form-control" value="{{$config ? $config->get('radioLabelNo') : ''}}" />
+        </div>
+    </div>
+</div>
+
+<div class="xe-form-group">
+    <label>{{ xe_trans('xe::description') }}</label>
+    <input type="text" name="skinDescription" placeholder="skin description" class="form-control" value="{{$config != null ? $config->get('skinDescription') : ''}}" />
+</div>
