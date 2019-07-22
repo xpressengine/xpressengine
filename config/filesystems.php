90ebb5bc58bc89cee7de62050f901f5af337b735
bnu <bnufactory@gmail.com>
2019-07-22 12:39:25 +0900

    media 디스크 url 경로 수정

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,7 +68,7 @@ return [
         'media' => [
             'driver' => 'local',
             'root' => storage_path('app/public/media'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'/storage/app/public/media',
             'visibility' => 'public',
         ],
 
