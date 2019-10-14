d86437d3a4adffec870186792ce0d397123d7da0
jhyeon1010 <cjh1010@xpressengine.com>
2019-10-14 16:28:30 +0900

    #1032 boolean 타입 확장 필드 검색값 유지 오류 수정

--- resources/views/dynamicField/boolean/default/search.blade.php
+++ resources/views/dynamicField/boolean/default/search.blade.php
@@ -1,6 +1,6 @@
 {{-- @deprecated .form-control --}}
 <select name="{{$key['boolean']}}" class="xe-form-control form-control __xe_df __xe_df_boolean __xe_df_boolean_{{$config->get('id')}}" data-valid-name="{{ xe_trans($config->get('label')) }}">
     <option value="">{{xe_trans($config->get('label'))}}</option>
-    <option value="1" @if($data['boolean'] === 1) selected @endif>{{ $config->get('radioLabelYes') ?: xe_trans('xe::yes') }}</option>
-    <option value="0" @if($data['boolean'] === 0) selected @endif>{{ $config->get('radioLabelNo') ?: xe_trans('xe::no') }}</option>
+    <option value="1" @if((int)$data['boolean'] === 1) selected @endif>{{ $config->get('radioLabelYes') ?: xe_trans('xe::yes') }}</option>
+    <option value="0" @if((int)$data['boolean'] === 0) selected @endif>{{ $config->get('radioLabelNo') ?: xe_trans('xe::no') }}</option>
 </select>
