8dd029ff9a984b53bc0da9d9551146ce5d960947
bnu <bnufactory@gmail.com>
2019-11-21 14:30:27 +0900

    회원 설정 페이지 일부 UI 되돌림

--- resources/views/user/settings/setting/terms/index.blade.php
+++ resources/views/user/settings/setting/terms/index.blade.php
@@ -1,109 +1,52 @@
 {{ XeFrontend::js('/assets/vendor/jqueryui/jquery-ui.min.js')->appendTo('head')->load() }}
-{{ XeFrontend::css('assets/core/xe-ui/css/xe-ui-without-base.css')->load() }}
 
-<div class="container-fluid container-fluid--all">
-    <div class="row">
-        <div class="col-sm-3" style="margin-bottom: 8px;">
-            <!-- 약관 항목 -->
-            <!-- 드래그 리스트 -->
-            <ul class="sort-list sort-list--button-type __ui-sortable">
-                <!-- [D] class="sort-list__button" 클릭 시 해당 부모 영역 li 태그에 class="on" 추가 (버튼 클릭 시 선택 표시) -->
-                {{-- @foreach ($terms as $item)
-                <li class="list-group-item">
-                    <button type="button" class="btn handler"><i class="xi-drag-vertical"></i></button>
-                    <span ><input type="checkbox" name="id[]" value="{{ $item->id }}"></span>
-                    <em class="item-title"><a href="{{ route('settings.user.setting.terms.edit', $item->id) }}">{{ xe_trans($item->title) }}</a></em>
-                    <div class="xe-btn-toggle pull-right">
-                        <label>
-                            <span class="sr-only">toggle</span>
-                            <input type="checkbox" name="enable[]" value="{{ $item->id }}" {{$item->is_enabled ? 'checked' : ''}}>
-                            <span class="toggle"></span>
-                        </label>
-                    </div>
-                </li>
-                @endforeach --}}
-                @foreach ($terms as $term)
-                <li>
-                    <div class="sort-list__handler">
-                        <button type="button" class="xu-button xu-button--subtle-link xu-button--icon __handler" data-term-id="{{ $term->id }}">
-                            <span class="xu-button__icon"><i class="xi-drag-vertical"></i></span>
-                        </button>
-                    </div>
-                    <button type="button" class="sort-list__button">{{ xe_trans($term->title) }}</button>
-                </li>
-                @endforeach
-            </ul>
 
-            <div class="setting-terms__sort-list-button">
-                <button type="button" class="xu-button xu-button--link" data-url="{{ route('settings.user.setting.terms.create') }}">
-                    <span class="xu-button__text">새 약관 만들기</span>
-                </button>
+<div class="panel-group">
+    <div class="panel">
+
+        <div class="panel-heading">
+            <div class="pull-left">
+                <h3 class="panel-title">{{xe_trans('xe::termsSettings')}}</h3>
             </div>
-            <!-- //약관 항목 -->
         </div>
+        <div class="panel-body">
+            <form id="index_form" method="post">
+                {{ csrf_field() }}
+                <div class="panel">
+                    <a href="{{ route('settings.user.setting.terms.create') }}" class="btn btn-primary"><i class="xi-plus"></i> <span>{{ xe_trans('xe::add') }}</span></a>
 
-        <div class="col-sm-9">
-            <div class="setting-area-group">
-                <!-- 각 세팅 박스 -->
-                <section class="setting-area">
-                    <div class="setting-area__body">
-                        <div class="setting-box">
-
-                            <div class="xu-form-group">
-                                <label>약관명</label>
-                                <!-- UI 오브젝트 영역(input) -->
-                                <input type="input" class="xu-form-group__control">
-                                <!-- //UI 오브젝트 영역(input) -->
-                            </div>
-                        </div>
-
-                        <div class="setting-box">
-                            <label class="xu-label-checkradio">
-                                <input type="radio">
-                                <span class="xu-label-checkradio__helper"></span>
-                                <span class="xu-label-checkradio__text">사용 <span class="xu-label-checkradio__empase">(필수)</span></span>
-                            </label>
-                            <label class="xu-label-checkradio">
-                                <input type="radio">
-                                <span class="xu-label-checkradio__helper"></span>
-                                <span class="xu-label-checkradio__text">사용 <span class="xu-label-checkradio__empase">(선택)</span></span>
-                            </label>
-                            <label class="xu-label-checkradio">
-                                <input type="radio">
-                                <span class="xu-label-checkradio__helper"></span>
-                                <span class="xu-label-checkradio__text">사용안함</span>
-                            </label>
-                        </div>
-
-                        <div class="setting-box">
-                            <div class="xu-form-group">
-                                <label>약관 설명</label>
-                                <!-- UI 오브젝트 영역(textarea) -->
-                                <textarea class="xu-form-group__control"></textarea>
-                                <!-- //UI 오브젝트 영역(textarea) -->
-                            </div>
-                        </div>
-
-                        <div class="setting-box">
-                            <div class="xu-form-group setting-box__terms-content">
-                                <!-- UI 오브젝트 영역(textarea) -->
-                                <textarea class="xu-form-group__control"></textarea>
-                                <!-- //UI 오브젝트 영역(textarea) -->
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                <!-- 전체 페이지 버튼 영역 -->
-                <div class="setting-button-box" style="text-align: right;">
-                    <button type="button" class="xu-button xu-button--default">삭제</button>
-                    <button type="button" class="xu-button xu-button--primary">저장하기</button>
-                    <button type="button" class="xu-button xu-button--primary">추가하기</button>
+                    <ul class="list-group item-setting">
+                        @foreach ($terms as $item)
+                            <li class="list-group-item">
+                                <button type="button" class="btn handler"><i class="xi-drag-vertical"></i></button>
+                                <span ><input type="checkbox" name="id[]" value="{{ $item->id }}"></span>
+                                <em class="item-title"><a href="{{ route('settings.user.setting.terms.edit', $item->id) }}">{{ xe_trans($item->title) }}</a></em>
+                                <div class="xe-btn-toggle pull-right">
+                                    <label>
+                                        <span class="sr-only">toggle</span>
+                                        <input type="checkbox" name="enable[]" value="{{ $item->id }}" {{$item->is_enabled ? 'checked' : ''}}>
+                                        <span class="toggle"></span>
+                                    </label>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
+            </form>
+
+        </div>
+        <div class="panel-footer">
+            <div class="pull-left">
+                <button type="button" class="btn btn-danger btn-lg btn-destroy">{{xe_trans('xe::deleteSelected')}}</button>
+            </div>
+            <div class="pull-right">
+                <button type="button" class="btn btn-primary btn-lg btn-submit">{{xe_trans('xe::save')}}</button>
             </div>
         </div>
+
     </div>
 </div>
+
 
 <script type="text/javascript">
     $(function() {
@@ -121,7 +64,7 @@
             }
 
             $('#index_form').attr('action', '{{ route('settings.user.setting.terms.destroies') }}')
-            .append('{{ method_field('delete') }}');
+                .append('{{ method_field('delete') }}');
             $('#index_form').submit();
         });
 
