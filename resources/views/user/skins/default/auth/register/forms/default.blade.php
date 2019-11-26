74c3e07088dc7ac2645c8f434842dec050428145
sirwoongke <fzwoongbin@naver.com>
2019-11-26 19:26:29 +0900

    회원가입 기본 스킨 스타일 적용

--- resources/views/user/skins/default/auth/register/forms/default.blade.php
+++ resources/views/user/skins/default/auth/register/forms/default.blade.php
@@ -7,18 +7,24 @@
         <input type="text" id="f-email" class="xu-form-group__control" placeholder="{{ xe_trans('xe::enterEmail') }}" name="email" value="{{ old('email') }}" required data-valid-name="{{ xe_trans('xe::email') }}">
     </div>
 </div>
-<div class="auth-group">
-    <label for="name" class="xe-sr-only">{{xe_trans('xe::name')}}</label>
-    <input type="text" id="name" class="xe-form-control" placeholder="{{xe_trans('xe::name')}}" name="display_name" value="{{ old('display_name') }}" data-valid-name="{{xe_trans('xe::name')}}">
+
+{{-- name --}}
+<div class="xu-form-group xu-form-group--large">
+    <label class="xu-form-group__label" for="f-name">{{ xe_trans('xe::name') }}<em class="xu-form-group__label-required">*</em></label>
+    <div class="xu-form-group__box">
+        <input type="text" id="f-name" class="xu-form-group__control" placeholder="{{ xe_trans('xe::enterName') }}" name="display_name" value="{{ old('display_name') }}" required data-valid-name="{{ xe_trans('xe::name') }}">
+    </div>
 </div>
-<div class="auth-group">
-    <label for="pwd" class="xe-sr-only">{{xe_trans('xe::password')}}</label>
-    <input type="password" id="pwd" class="xe-form-control" placeholder="{{xe_trans('xe::password')}}" name="password" onfocus="$('.__xe_password-description').slideDown();" data-valid-name="{{xe_trans('xe::password')}}">
-    <em style="display: none;" class="text-message __xe_password-description">{{ $passwordValidator->getMessage() }}</em>
-</div>
-<div class="auth-group">
-    <label for="pwd2" class="xe-sr-only">{{xe_trans('xe::passwordConfirm')}}</label>
-    <input type="password" id="pwd2" class="xe-form-control" placeholder="{{xe_trans('xe::passwordConfirm')}}" name="password_confirmation">
+
+{{-- password --}}
+<div class="xu-form-group xu-form-group--large">
+    <label class="xu-form-group__label" for="f-password">{{ xe_trans('xe::password') }}<em class="xu-form-group__label-required">*</em></label>
+        <div class="xu-form-group__box xu-form-group__box--icon-right">
+            <input type="password" id="f-password" class="xu-form-group__control" placeholder="{{ xe_trans('xe::enterPassword') }}" name="password" required data-valid-name="{{xe_trans('xe::password')}}">
+            <button type="button" class="xu-form-group__icon __xe-toggle-password">
+                <i class="xi-eye"></i>
+            </button>
+    </div>
 </div>
 
 <script>
--- resources/views/user/skins/default/auth/register/forms/dfields.blade.php
+++ resources/views/user/skins/default/auth/register/forms/dfields.blade.php
@@ -1,7 +1,6 @@
 @if (count($fields) > 0)
-    <h3>{{ xe_trans('xe::additionalInfo') }}</h3>
     @foreach($fields as $field)
-        <div class="control-group xu-form-group">
+        <div class="control-group xu-form-group xu-form-group--large">
             {!! $field->getSkin()->create(request()->all()) !!}
         </div>
     @endforeach
