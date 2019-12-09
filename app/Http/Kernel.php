8c86bf38478036689e92d505513033512f74a819
akasima <akasima@gmail.com>
2019-12-09 06:04:28 +0000

    #1086 HttpProtocol 추가 및 ssl always 설정 추가

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -73,6 +73,7 @@ class Kernel extends HttpKernel
             // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
+            \App\Http\Middleware\HttpProtocol::class,
             \App\Http\Middleware\RequiredDF::class,
             \App\Http\Middleware\ExceptAppendableVerifyCsrfToken::class,
             \App\Http\Middleware\LangPreprocessor::class,
---
+++ app/Http/Middleware/HttpProtocol.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Purifying.php
+ *
+ * PHP version 7
+ *
+ * @category    Middleware
+ * @package     App\Http\Middleware
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2019 Copyright XEHub Corp. <https://www.xehub.io>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
+ * @link        https://xpressengine.io
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+/**
+ * Class HttpProtocol
+ *
+ * @category    Middleware
+ * @package     App\Http\Middleware
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2019 Copyright XEHub Corp. <https://www.xehub.io>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
+ * @link        https://xpressengine.io
+ */
+class HttpProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (app('config')->get('xe.ssl.always') == true && $request->secure() == false) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
