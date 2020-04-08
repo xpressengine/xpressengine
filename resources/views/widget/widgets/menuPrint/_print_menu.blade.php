c9f54c171205170dd869d9e716b9e2c02b5a0528
bnu <bnufactory@gmail.com>
2020-04-08 11:50:45 +0900

    #1127 menuPrint 위젯 기본 마크업 정리
    - nav 태그 제거
    - class 이름 정리
    - _blank 타겟 지정 항목에 noopener 등 추가

--- resources/views/widget/widgets/menuPrint/_print_menu.blade.php
+++ resources/views/widget/widgets/menuPrint/_print_menu.blade.php
@@ -1,15 +1,20 @@
-<ul class="@if($__depth == 1) gnb-menu @else sub-menu @endif depth-{{$__depth}}">
+<ul class="xe-widget xe-widget-type--menuprint __xe-widget xe-menu @if($__depth == 1) xe-menu--top @else xe-menu--sub @endif xe-menu--depth-{{$__depth}}" @if($__depth == 1)data-widget-id="{{$config['menu_id']}}" data-widget="menuPrint" @endif>
     @foreach ($__menuList as $menu)
-        <li class="depth-{{$__depth}}" data-menu-type="{{$menu['type']}}">
-            <a href="{{ url($menu['url']) }}" class="menu-link @if($__depth == 1) gnb-menu-link @else sub-menu-link @endif @if ($menu['target'] == '_blank') target-blank @endif @if ($menu['url'] === '#') inner-link @endif " @if ($menu['target'] !== '_self') target="{{ $menu['target'] }}" @endif>
-                <span class="link-text">{{ $menu['link'] }}@if ($menu['target'] == '_blank') <i class="xi-external-link"></i>@endif</span>
+        <li class="xe-menu__item xe-menu--depth-{{$__depth}}" data-menu-type="{{ $menu['type'] }}">
+            <a href="{{ url($menu['url']) }}"
+                class="xe-menu__link @if($__depth == 1) xe-menu__link--top @else xe-menu__link--sub @endif @if($menu['target'] == '_blank') xe-menu__link--target-new @endif @if($menu['url'] === '#') xe-menu__link--target-bookmark @endif"
+                @if($menu['target'] !== '_self') target="{{ $menu['target'] }}" @endif
+                @if($menu['target'] === '_blank') rel="noreferrer noopener" @endif
+            >
+                <span class="xe-menu__text">{{ $menu['link'] }}</span>
             </a>
+
+            @if(isset($menu['children']) && count($menu['children']) && $__depth < $config['limit_depth'])
+                @include($_skin::getPath() . '._print_menu', [
+                '__menuList' => $menu['children'],
+                '__depth' => $__depth + 1,
+            ])
         </li>
-        @if (isset($menu['children']) && count($menu['children']) && $__depth < $config['limit_depth'])
-            @include($_skin::getPath() . '._print_menu', [
-            '__menuList' => $menu['children'],
-            '__depth' => $__depth + 1,
-        ])
         @endif
     @endforeach
 </ul>
