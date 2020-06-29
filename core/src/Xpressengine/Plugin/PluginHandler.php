3e1e74763d50352c67af098ecd37f382c0c409ed
Darron Park <darron1217@gmail.com>
2020-06-29 18:17:06 +0900

    PluginNotFoundException에서 plugin이름이 :plugin으로 나오는 버그 수정 (#1172)

--- core/src/Xpressengine/Plugin/PluginHandler.php
+++ core/src/Xpressengine/Plugin/PluginHandler.php
@@ -238,7 +238,7 @@ class PluginHandler
 
         // 플러그인이 존재하는지 검사한다.
         if ($entity === null) {
-            throw new PluginNotFoundException(['pluginName' => $pluginId]);
+            throw new PluginNotFoundException(['plugin' => $pluginId]);
         }
 
         // 플러그인이 이미 활성화되어있는 상태인지 체크한다.
