f50846eb19b7c83cc745ad6dce56c578315c37a7
ParkHongSic <gpo04174@naver.com>
2020-11-11 11:47:58 +0900

    2020-11-11 윈도우 환경 플어그인 버그 수정 외 1 윈도우 환경 플러그인 스캔 버그 추가, .env 테이터베이스 설정에 DB_PREFIX 추가

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            'prefix' => '',
+            'prefix' => env('DB_PREFIX', ''),
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
@@ -84,7 +84,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            'prefix' => '',
+            'prefix' => env('DB_PREFIX', ''),
         ],
 
         'cubrid' => [
@@ -95,7 +95,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            'prefix' => '',
+            'prefix' => env('DB_PREFIX', ''),
         ],
     ],
 
