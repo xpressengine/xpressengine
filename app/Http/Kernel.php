72d39eba8265ff2a825a9c2b25608a7237ea9bd0
xiso <xiso@amuz.co.kr>
2021-01-29 23:06:17 +0900

    크로스도메인 이슈추가

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,6 +50,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\Cors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
