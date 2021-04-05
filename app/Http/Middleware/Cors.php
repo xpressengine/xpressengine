a32307b61afb331e7a719281c4efe9660f6c1e60
xiso <xiso@amuz.co.kr>
2021-04-05 18:49:01 +0900

    로드밸런싱 이슈 fix index로 이전

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Middleware;
 use Closure;
+use \Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 
 class Cors
@@ -12,21 +13,8 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        //SSL Load balancing fix
-        if ((isset($_ENV["HTTPS"]) && ("on" == $_ENV["HTTPS"]))
-            || (isset($_SERVER["HTTP_X_FORWARDED_SSL"]) && (strpos($_SERVER["HTTP_X_FORWARDED_SSL"], "1") !== false))
-            || (isset($_SERVER["HTTP_X_FORWARDED_SSL"]) && (strpos($_SERVER["HTTP_X_FORWARDED_SSL"], "on") !== false))
-            || (isset($_SERVER["HTTP_CF_VISITOR"]) && (strpos($_SERVER["HTTP_CF_VISITOR"], "https") !== false))
-            || (isset($_SERVER["HTTP_CLOUDFRONT_FORWARDED_PROTO"]) && (strpos($_SERVER["HTTP_CLOUDFRONT_FORWARDED_PROTO"], "https") !== false))
-            || (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && (strpos($_SERVER["HTTP_X_FORWARDED_PROTO"], "https") !== false))
-            || (isset($_SERVER["HTTP_X_PROTO"]) && (strpos($_SERVER["HTTP_X_PROTO"], "SSL") !== false))
-        ) {
-            $_SERVER["HTTPS"] = "on";
-            URL::forceScheme('https');
-        }
-
         return $next($request)
             ->header("Access-Control-Allow-Origin", "*")
             ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
