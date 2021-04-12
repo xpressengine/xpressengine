f0ed0bb11f5a14011c53d020d676140b26e21577
xiso <xiso@amuz.co.kr>
2021-04-12 18:51:54 +0900

    약관 미리보기 줄바꿈문제 수정

--- resources/views/terms/index.blade.php
+++ resources/views/terms/index.blade.php
@@ -1,7 +1,9 @@
-<div class="container" style="margin:20px 0;">
-<h2>{{ xe_trans($term->title) }}</h2>
-<hr />
-<div style="padding:20px; background:#eee; border:1px solid #ccc;">
-    {!! nl2br(xe_trans($term->content)) !!}
-</div>
+<div class="container">
+    <div  style="margin:20px 0;">
+        <h2>{{ xe_trans($term->title) }}</h2>
+        <hr />
+        <div style="padding:20px; background:#eee; border:1px solid #ccc;">
+            {!! nl2br(xe_trans($term->content)) !!}
+        </div>
+    </div>
 </div>
