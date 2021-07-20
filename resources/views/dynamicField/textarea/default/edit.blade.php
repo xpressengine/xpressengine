b475a69dc86f81dc5814eacb98c39abed46df42e
xharpenParksuhyeon <parksu@xharpen.com>
2021-07-20 16:52:00 +0000

    #1249 'Textarea - 긴 문자' 확장 필드의 기본 스킨에 rows, cols를 설정할 수 있게 수정

--- resources/views/dynamicField/textarea/default/edit.blade.php
+++ resources/views/dynamicField/textarea/default/edit.blade.php
@@ -1,6 +1,12 @@
 <div class="xe-form-group xe-dynamicField">
     <label class="xu-form-group__label __xe_df __xe_df_textarea __xe_df_textarea_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
     @if ($config->get('skinDescription') !== '')<small>{{$config->get('skinDescription')}}</small>@endif
-    <textarea type="text" name="{{$key['text']}}" class="xe-form-control xu-form-group__control __xe_df __xe_df_textarea __xe_df_textarea_{{$config->get('id')}}"
-    placeholder="{{xe_trans($config->get('placeholder', ''))}}" data-valid-name="{{ xe_trans($config->get('label')) }}" />{{$data['text']}}</textarea>
+    <textarea
+        type="text" name="{{$key['text']}}"
+        class="xe-form-control xu-form-group__control __xe_df __xe_df_textarea __xe_df_textarea_{{$config->get('id')}}"
+        placeholder="{{xe_trans($config->get('placeholder', ''))}}"
+        data-valid-name="{{ xe_trans($config->get('label')) }}"
+        @if($config->get('skinRows') !== null && $config->get('skinRows') > 0) rows="{{ $config->get('skinRows') }}" @endif
+        @if($config->get('skinCols') !== null && $config->get('skinCols') > 0) cols="{{ $config->get('skinCols') }}" @endif
+    />{{$data['text']}}</textarea>
 </div>
--- resources/views/dynamicField/textarea/default/settings.blade.php
+++ resources/views/dynamicField/textarea/default/settings.blade.php
@@ -1,1 +1,11 @@
-<input type="text" name="skinDescription" placeholder="skin description" class="form-control" value="{{$config != null ? $config->get('skinDescription') : ''}}" />
+<div>
+    <input type="text" name="skinDescription" placeholder="skin description" class="form-control" value="{{$config != null ? $config->get('skinDescription') : ''}}" />
+</div>
+
+<div style="margin-top: 10px;">
+    <input type="number" min="1" name="skinRows" placeholder="skin textarea rows attribute" class="form-control" value="{{ $config != null ? $config->get('skinRows') : '' }}" />
+</div>
+
+<div style="margin-top: 10px;">
+    <input type="number" min="1" name="skinCols" placeholder="skin textarea cols attribute" class="form-control" value="{{ $config != null ? $config->get('skinCols') : '' }}" />
+</div>
