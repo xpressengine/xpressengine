f119b9d2ea674c8fa3220a4937b48fd1ceee9f22
xharpenParksuhyeon <parksu@xharpen.com>
2021-07-23 09:04:52 +0000

    #1261 'Url - 도메인 주소' - 'Url default' 스킨에서 링크 활성화 여부 결정하는 필드 추가

--- resources/views/dynamicField/url/default/settings.blade.php
+++ resources/views/dynamicField/url/default/settings.blade.php
@@ -1,1 +1,12 @@
-<input type="text" name="skinDescription" placeholder="skin description" class="form-control" value="{{$config != null ? $config->get('skinDescription') : ''}}" />
+<div>
+    <label class="__xe_df">Skin Description</label>
+    <input type="text" name="skinDescription" placeholder="skin description" class="form-control" value="{{$config != null ? $config->get('skinDescription') : ''}}" />
+</div>
+
+<div style="margin-top: 10px">
+    <label class="__xe_df">Href Activate</label>
+    <select name="skinHrefActivate" class="form-control">
+        <option value="off" @if($config !== null && $config->get('skinHrefActivate') == 'off') selected  @endif>off</option>
+        <option value="on" @if($config !== null && $config->get('skinHrefActivate') == 'on') selected  @endif>on</option>
+    </select>
+</div>
--- resources/views/dynamicField/url/default/show.blade.php
+++ resources/views/dynamicField/url/default/show.blade.php
@@ -1,4 +1,14 @@
 <div class="xe-form-group xe-dynamicField">
     <label class="__xe_df __xe_df_url __xe_df_url_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
+
+    @if ($config->get('skinHrefActivate') === 'on')
+    <a href="{{ $data['url'] }}">
+    @endif
+
     <span class="__xe_df __xe_df_url __xe_df_url_{{$config->get('id')}}">{{$data['url']}}</span>
+
+    @if ($config->get('skinHrefActivate') === 'on')
+    </a>
+    @endif
 </div>
+
