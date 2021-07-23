3dc070ab69b61e3129683383c565a2b80b767f38
xharpenParksuhyeon <parksu@xharpen.com>
2021-07-23 09:14:57 +0000

    #1262 'Url - 도메인 주소' - 'Url default' 스킨에서 target Attribute 설정하는 필드 추가.

--- resources/views/dynamicField/url/default/settings.blade.php
+++ resources/views/dynamicField/url/default/settings.blade.php
@@ -10,3 +10,11 @@
         <option value="on" @if($config !== null && $config->get('skinHrefActivate') == 'on') selected  @endif>on</option>
     </select>
 </div>
+
+<div style="margin-top: 10px">
+    <label class="__xe_df">Href Target</label>
+    <select name="skinHrefTarget" class="form-control">
+        <option value="_self" @if($config !== null && $config->get('skinHrefTarget') == '_self') selected  @endif>_self</option>
+        <option value="_blank" @if($config !== null && $config->get('skinHrefTarget') == '_blank') selected  @endif>_blank</option>
+    </select>
+</div>
--- resources/views/dynamicField/url/default/show.blade.php
+++ resources/views/dynamicField/url/default/show.blade.php
@@ -2,7 +2,7 @@
     <label class="__xe_df __xe_df_url __xe_df_url_{{$config->get('id')}}">{{xe_trans($config->get('label'))}}</label>
 
     @if ($config->get('skinHrefActivate') === 'on')
-    <a href="{{ $data['url'] }}">
+    <a href="{{ $data['url'] }}" @if ($config->get('skinHrefTarget')) target="{{ $config->get('skinHrefTarget') }}" @endif>
     @endif
 
     <span class="__xe_df __xe_df_url __xe_df_url_{{$config->get('id')}}">{{$data['url']}}</span>
