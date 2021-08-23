4ae961db79f23e84ea431bf18d11f27e9c9fe3b4
Bi Kim <kimbi@outlook.kr>
2021-08-23 16:51:09 +0900

    (#1340) 이용약관 불필요한 인라인 스타일 제거 후 클래스로 대체

--- resources/views/terms/index.blade.php
+++ resources/views/terms/index.blade.php
@@ -1,8 +1,8 @@
 <div class="container">
-    <div  style="margin:20px 0;">
+    <div style="margin:20px 0;">
         <h2>{{ xe_trans($term->title) }}</h2>
         <hr />
-        <div style="padding:20px; background:#eee; border:1px solid #ccc;">
+        <div class="xf-term-content">
             {!! nl2br(xe_trans($term->content)) !!}
         </div>
     </div>
