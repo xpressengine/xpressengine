71c4277ce70b797f2b6fc0fb328cf597b9cc8479
박종근 <postman@xpressengine.com>
2022-05-25 11:59:49 +0900

    프로필에 대한 수정 권한 있을 때만 닉네임 입력 창의 Outline이 노출되도록 수정 #1405

--- resources/views/user/skins/default/profile/index.blade.php
+++ resources/views/user/skins/default/profile/index.blade.php
@@ -22,7 +22,9 @@
                                 @endif
                             </dd>
                             <dt class="xe-sr-only"><label for="nick">{{xe_trans('xe::displayName')}}</label></dt>
-                            <dd class="input-nick"><input class="__xe_nameInput" type="text" name="display_name" placeholder="{{xe_trans('xe::enterName')}}" value="{{ $user->getDisplayName() }}" readonly></dd>
+                            <dd class="input-nick">
+                                <input class="__xe_nameInput" type="text" name="display_name" placeholder="{{xe_trans('xe::enterName')}}" value="{{ $user->getDisplayName() }}" readonly @if($grant['modify'] === false) style="outline: none" @endif>
+                            </dd>
 
                             @if($grant['manage'])
                                 <dt class="xe-sr-only">{{xe_trans('xe::group')}}</dt>
