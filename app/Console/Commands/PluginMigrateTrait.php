7fc7aa5e15b03b594e2e01466d000bc9b537d2e7
xharpenParksuhyeon <parksu@xharpen.com>
2022-09-12 14:34:23 +0000

    [laravel 5.7] plugin migrate에서 발생하는 오류 해결 (마이그레이션 명령 출력) / realpath 옵션 추가

--- app/Console/Commands/PluginMigrateTrait.php
+++ app/Console/Commands/PluginMigrateTrait.php
@@ -2,9 +2,20 @@
 
 namespace App\Console\Commands;
 
+use Exception;
+use Illuminate\Filesystem\Filesystem;
+use RuntimeException;
+use SplFileInfo;
 use Symfony\Component\Console\Input\InputArgument;
 use Xpressengine\Plugin\Migrations\PluginMigrator;
+use Xpressengine\Plugin\PluginEntity;
+use Xpressengine\Plugin\PluginHandler;
 
+/**
+ * Trait PluginMigrateTrait
+ *
+ * @package App\Console\Commands
+ */
 trait PluginMigrateTrait
 {
     /**
@@ -15,39 +26,51 @@ trait PluginMigrateTrait
     protected $migrator;
 
     /**
+     * @var $handler
+     */
+    protected $handler;
+
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
      * Create a new migration rollback command instance.
      *
      * @param  PluginMigrator  $migrator
-     * @return void
      */
     public function __construct(PluginMigrator $migrator)
     {
         parent::__construct($migrator);
 
-        $this->migrator = $migrator;
+        $this->handler = app(PluginHandler::class);
+        $this->filesystem = app(Filesystem::class);
     }
 
     /**
      * Get the path to the migration directory.
      *
      * @return string
+     * @throws Exception
      */
     protected function getMigrationPath()
     {
         $pluginId = $this->argument('plugin');
-        // 플러그인이 이미 설치돼 있는지 검사
-        if (!$plugin = app('xe.plugin')->getPlugin($pluginId)) {
-            // 설치되어 있지 않은 플러그인입니다.
-            throw new \Exception('Plugin not found');
+        $plugin = $this->handler->getPlugin($pluginId);
+
+        if (($plugin instanceof PluginEntity) === false) {
+            throw new RuntimeException('Plugin not found');
         }
 
-        $migrationPath = str_replace(base_path().DIRECTORY_SEPARATOR, '', $plugin->getPath()).DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations';
+        $pluginPath = str_replace(base_path().DIRECTORY_SEPARATOR, '', $plugin->getPath());
+        $pluginMigrationPath = $pluginPath.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations';
 
-        if (!app('files')->exists($migrationPath)) {
-            throw new \Exception('Directory ['.$migrationPath.'] does not exist');
+        if ($this->filesystem->exists($pluginMigrationPath) === false) {
+            throw new RuntimeException('Directory ['.$pluginMigrationPath.'] does not exist');
         }
 
-        return $migrationPath;
+        return $pluginMigrationPath;
     }
 
     /**
@@ -64,15 +87,20 @@ trait PluginMigrateTrait
 
     /**
      * 플러그인의 마이그레이션 파일목록을 확장자 없이 가져옴
+     *
      * @return array
-     * @throws \Exception
+     * @throws Exception
      */
     protected function getFileList()
     {
-        $fileList = app('files')->allFiles($this->getMigrationPath());
+        $fileList = $this->filesystem->allFiles($this->getMigrationPath());
 
-        return collect($fileList)->map(function(\SplFileInfo $file) {
-            return $file->getBasename('.' . $file->getExtension());
-        })->all();
+        $fileList = collect($fileList)->map(
+            function (SplFileInfo $file) {
+                return $file->getBasename('.'.$file->getExtension());
+            }
+        );
+
+        return $fileList->all();
     }
 }
