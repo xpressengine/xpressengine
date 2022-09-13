a0661a4ea196a6b5396f2b4151f9bfe6d47f79d3
xharpenParksuhyeon <parksu@xharpen.com>
2022-09-13 02:17:05 +0000

    [laravel 6.x] config/database.php 변경- cubrid 설정 복구

--- config/database.php
+++ config/database.php
@@ -98,7 +98,19 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-        ]
+        ],
+
+        'cubrid' => [
+            'driver' => 'cubrid',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '30000'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+        ],
     ],
 
     /*
