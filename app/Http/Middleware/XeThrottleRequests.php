bc2a42381ca0e7d89f4de684aabaf5bb8d561b59
gyorudayo <khg7001@gmail.com>
2022-12-13 23:45:26 +0900

    - #1446 API Throttling 처리 시 사용되는 사용자 인증 로직이 비로그인 상태에서 비정상적으로 동작하는 문제 수정

---
+++ app/Http/Middleware/XeThrottleRequests.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * XeThrottleRequests.php
+ *
+ * PHP version 7
+ *
+ * @category    Middleware
+ * @package     App\Http\Middleware
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2020 Copyright XEHub Corp. <https://www.xehub.io>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
+ * @link        https://xpressengine.io
+ */
+
+namespace App\Http\Middleware;
+
+use RuntimeException;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+
+/**
+ * Class XeThrottleRequests
+ *
+ * @category    Middleware
+ * @package     App\Http\Middleware
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2020 Copyright XEHub Corp. <https://www.xehub.io>
+ * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
+ * @link        https://xpressengine.io
+ */
+class XeThrottleRequests extends ThrottleRequests
+{
+    /**
+     * Resolve request signature.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     * @throws \RuntimeException
+     */
+    protected function resolveRequestSignature($request)
+    {
+        // Check current user is XpressEngine user (not a guest).
+        $user = $request->user();
+        if ($user instanceof \Xpressengine\User\Models\User) {
+            return sha1($user->getAuthIdentifier());
+        }
+
+        if ($route = $request->route()) {
+            return sha1($route->getDomain().'|'.$request->ip());
+        }
+
+        throw new RuntimeException(
+            'Unable to generate the request signature. Route unavailable.'
+        );
+    }
+}
